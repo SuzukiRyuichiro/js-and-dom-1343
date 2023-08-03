349076b0198abf3d5ba8413a2e121619928b915f
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-03 11:16:41 +0100

    adjust

--- javascript/application.js
+++ javascript/application.js
@@ -1,10 +1,17 @@
-// const list = document.querySelector("#players");
-// const jedi = list.querySelector(".red");
+const list = document.querySelector("#players");
+const jedi = list.querySelector(".red");
 
-// console.log(jedi);
+console.log(jedi);
 
 const countries = document.querySelectorAll("#fifa-wins li");
 
 countries.forEach((country) => {
   console.log(country.innerText);
 });
+
+const soccerList = document.querySelector("#fifa-wins");
+soccerList.insertAdjacentHTML("beforeend", "<li>France (2 wins)");
+
+const input = document.querySelector("#input");
+console.log(input.value);
+console.log(input.id);
