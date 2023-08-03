e726cefb76afa1d7977fc660886dd0f09217b86e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-03 11:14:37 +0100

    done

---
+++ javascript/application.js
@@ -0,0 +1,10 @@
+// const list = document.querySelector("#players");
+// const jedi = list.querySelector(".red");
+
+// console.log(jedi);
+
+const countries = document.querySelectorAll("#fifa-wins li");
+
+countries.forEach((country) => {
+  console.log(country.innerText);
+});
